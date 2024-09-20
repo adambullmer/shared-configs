28ae433f4311e1c04f2cdf98df60c0f5bb544aa2
Adam Bullmer <psycodrumfreak@gmail.com>
2024-09-20 05:58:38 +0000

    :sparkles:feat: Add commitlint shared config package

---
+++ packages/commitlint/README.md
@@ -0,0 +1,13 @@
+# commitlint
+
+> Sharable config for [commitlint](https://commitlint.js.org/)
+
+## Using the config
+
+Install with your preferred package manager as a devDependency.
+Then export this package from your preferred [commitlint config file location](https://commitlint.js.org/reference/configuration.html#config-via-file).
+For simplicity, the below example assumes your config file is `commitlint.config.mjs`.
+
+```sh
+echo 'export { default as default } from "@adambullmer/commitlint-config";' > commitlint.config.mjs
+```
---
+++ packages/commitlint/commitlint.config.mjs
@@ -0,0 +1,19 @@
+import { RuleConfigSeverity } from "@commitlint/types";
+import semanticCommitEmojiPreset from "commitlint-config-semantic-commit-emoji";
+import { parserPreset } from "commitlint-plugin-semantic-commit-emoji/parser";
+
+/**
+ * @type {import('@commitlint/types').UserConfig}
+ */
+const config = {
+  extends: ["@commitlint/config-conventional", "semantic-commit-emoji"],
+  parserPreset: parserPreset,
+  rules: {
+    ...semanticCommitEmojiPreset.rules,
+    "subject-case": [RuleConfigSeverity.Error, "always", "sentence-case"],
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", Infinity],
+    "footer-max-line-length": [RuleConfigSeverity.Error, "always", Infinity],
+  },
+};
+
+export default config;

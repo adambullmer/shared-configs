93005b96b666434fe6743ea1d46e40667013a8e7
Adam Bullmer <psycodrumfreak@gmail.com>
2024-09-23 04:20:16 +0000

    :sparkles:feat: Initialize biome config

---
+++ packages/biome/README.md
@@ -0,0 +1,13 @@
+# biome
+
+> Sharable config for [biome](https://biomejs.dev/)
+
+## Using the config
+
+Install with your preferred package manager as a devDependency.
+Then export this package from your preferred [biome config file location](https://biomejs.dev/guides/configure-biome/#configuration-file-structure).
+For simplicity, the below example assumes your config file is `biome.jsonc`.
+
+```sh
+echo "{\n  \"extends\": [\"@adambullmer/biome-config\"]\n}" > biome.jsonc
+```
---
+++ packages/biome/release.config.mjs
@@ -0,0 +1,49 @@
+import { parserPreset } from "commitlint-plugin-semantic-commit-emoji/parser";
+
+// Requires a first party dependency on conventional-changelog-conventionalcommits
+const preset = "conventionalcommits";
+
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  extends: "semantic-release-monorepo",
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset,
+        parserOpts: parserPreset.parserOpts,
+        releaseRules: [
+          { type: "types", scope: "*", release: "minor" },
+          { type: "refactor", release: "minor" },
+          { type: "style", release: "patch" },
+          { type: "hotfix", release: "patch" },
+          { type: "locale", release: "patch" },
+          { type: "docs", scope: "*", release: "patch" },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset,
+        parserOpts: parserPreset.parserOpts,
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["biome.jsonc"],
+      },
+    ],
+  ],
+};
